f9a9fbc34806d045d2c67c29de739bb3a631bef5
Vinícius Daniel <dannful@gmail.com>
2024-07-08 19:12:56 -0300

    Attempt to fix encryption error

--- vaxsul_frontend/src/lib/encryption.ts
+++ vaxsul_frontend/src/lib/encryption.ts
@@ -1,9 +1,10 @@
+import crypto from "crypto";
+
 export async function sha256(message: string) {
   const msgBuffer = new TextEncoder().encode(message);
-  const hashBuffer = await crypto.subtle.digest("SHA-256", msgBuffer);
-  const hashArray = Array.from(new Uint8Array(hashBuffer));
-  const hashHex = hashArray
-    .map((b) => b.toString(16).padStart(2, "0"))
-    .join("");
-  return hashHex;
+  const hashBuffer = crypto
+    .createHash("sha256")
+    .update(msgBuffer)
+    .digest("hex");
+  return hashBuffer;
 }
