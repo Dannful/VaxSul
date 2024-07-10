f2ee1ea35d124dc1754e7852349f82204e2bc9f4
Vinícius Daniel <dannful@gmail.com>
2024-07-10 20:26:54 -0300

    Including backend IP in workflow

--- vaxsul_frontend/src/app/layout.tsx
+++ vaxsul_frontend/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import StoreProvider from "./StoreProvider";
+import { PublicEnvScript } from "next-runtime-env";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,6 +19,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <PublicEnvScript />
+      </head>
       <StoreProvider>
         <body className={inter.className}>{children}</body>
       </StoreProvider>
--- vaxsul_frontend/src/service/vaxsul.ts
+++ vaxsul_frontend/src/service/vaxsul.ts
@@ -1,11 +1,12 @@
 import { Credentials } from "@/types/credentials";
 import { User } from "@/types/user";
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { env } from "next-runtime-env";
 
 export const vaxSulApi = createApi({
   reducerPath: "vaxSulApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: process.env.NEXT_PUBLIC_VAXSUL_SERVER_URL,
+    baseUrl: env("NEXT_PUBLIC_VAXSUL_SERVER_URL"),
   }),
   endpoints: (builder) => ({
     login: builder.mutation<string, Credentials>({
