7a7c892224efcdb227cbeaec85d14619c2952750
Mateus Fauri <faurimateus@gmail.com>
2024-07-10 18:56:25 -0300

    changes to the front end of views

--- vaxsul_frontend/src/app/login/page.tsx
+++ vaxsul_frontend/src/app/login/page.tsx
@@ -4,92 +4,117 @@ import { useState } from "react";
 import { FetchBaseQueryError } from "@reduxjs/toolkit/query";
 import { SerializedError } from "@reduxjs/toolkit";
 import { sha256 } from "@/lib/encryption";
+import Image from 'next/image';
+import vaxsulLogo from '../images/vaxsul.jpg';
+import Link from 'next/link';
 
 export default function Login() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [login, result] = useLoginMutation();
+
   return (
-    <form
-      className="max-w-sm mx-auto flex justify-center items-center h-screen"
-      onSubmit={async (e) => {
-        e.preventDefault();
-        login({
-          username: username,
-          password: await sha256(password),
-        })
-          .unwrap()
-          .then((token) => {
-            alert(token);
-          });
-      }}
-    >
-      <div className="w-full">
-        {result.isError && (
-          <div
-            className="p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400"
-            role="alert"
+    <div className="min-h-screen flex flex-col relative" style={{ background: "#1e426f", backgroundImage: "linear-gradient(to right, #1e426f 50%, #33639e)" }}>
+      <div className="flex justify-end p-4 items-center">
+        <span className="text-white mr-2">Não tem uma conta?</span>
+        <Link href="/register">
+          <button
+            className="text-white bg-white bg-opacity-20 hover:bg-opacity-30 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 transition duration-150 ease-in-out"
           >
-            {getMessageForStatusCode(result.error)}
-          </div>
-        )}
-        <div className="mb-5">
-          <label
-            htmlFor="email"
-            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-          >
-            E-mail
-          </label>
-          <input
-            type="email"
-            id="email"
-            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            placeholder="example@email.com"
-            required
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-          />
-        </div>
-        <div className="mb-5">
-          <label
-            htmlFor="password"
-            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-          >
-            Senha
-          </label>
-          <input
-            type="password"
-            id="password"
-            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            required
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </div>
-        <div className="flex items-start mb-5">
-          <div className="flex items-center h-5">
-            <input
-              id="remember"
-              type="checkbox"
-              value=""
-              className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800"
-            />
-          </div>
-          <label
-            htmlFor="remember"
-            className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-          >
-            Remember me
-          </label>
-        </div>
-        <button
-          type="submit"
-          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-        >
-          Enviar
-        </button>
+            Cadastrar
+          </button>
+        </Link>
       </div>
-    </form>
+      <div className="flex flex-grow items-center justify-between mx-24">
+        <div className="flex items-center justify-start w-1/3">
+          <Image src={vaxsulLogo} alt="Vaxsul Logo" width={300} height={300} className="object-contain" />
+        </div>
+        <div className="absolute inset-y-20 left-1/2 bg-gray-400 w-px opacity-20 transform -translate-x-1/2"></div> {}
+        <div className="w-2/5 bg-white bg-opacity-30 p-10 rounded-lg shadow-lg">
+          <h2 className="text-2xl font-bold text-gray-200 mb-6 text-center">Bem-vindo</h2>
+          <form
+            onSubmit={async (e) => {
+              e.preventDefault();
+              login({
+                username: username,
+                password: await sha256(password),
+              })
+                .unwrap()
+                .then((token) => {
+                  alert(token);
+                });
+            }}
+          >
+            {result.isError && (
+              <div
+                className="p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400"
+                role="alert"
+              >
+                {getMessageForStatusCode(result.error)}
+              </div>
+            )}
+            <div className="mb-6">
+              <label
+                htmlFor="email"
+                className="block mb-2 text-sm font-medium text-gray-200"
+              >
+                E-mail
+              </label>
+              <input
+                type="email"
+                id="email"
+                className="bg-gray-50 bg-opacity-40 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-3 placeholder-gray-500"
+                placeholder="example@email.com"
+                required
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              />
+            </div>
+            <div className="mb-6">
+              <label
+                htmlFor="password"
+                className="block mb-2 text-sm font-medium text-gray-200"
+              >
+                Senha
+              </label>
+              <input
+                type="password"
+                id="password"
+                className="bg-gray-50 bg-opacity-40 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-3"
+                required
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+            <div className="flex justify-between items-center mb-6">
+              <div className="flex items-center">
+                <input
+                  id="remember"
+                  type="checkbox"
+                  className="w-4 h-4 border border-gray-300 rounded bg-gray-50 bg-opacity-40 focus:ring-3 focus:ring-blue-300"
+                />
+                <label
+                  htmlFor="remember"
+                  className="ml-2 text-sm font-medium text-gray-200"
+                >
+                  Lembrar-me
+                </label>
+              </div>
+              <Link href="/forgot-password" className="text-sm text-blue-200 hover:underline">
+                Esqueceu a senha?
+              </Link>
+            </div>
+            <button
+              type="submit"
+              className="text-white bg-[#9dca46] bg-opacity-90 hover:bg-opacity-50 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full px-5 py-3 text-center transition duration-150 ease-in-out transform hover:scale-105"
+            >
+              Entrar
+            </button>
+          </form>
+        </div>
+      </div>
+      <div className="absolute inset-y-20 left-1/2 bg-gray-400 w-px opacity-40 transform -translate-x-1/2"></div> { }
+    </div>
   );
 }
 
--- vaxsul_frontend/src/app/register/page.tsx
+++ vaxsul_frontend/src/app/register/page.tsx
@@ -1,104 +1,205 @@
 "use client";
 import { sha256 } from "@/lib/encryption";
 import { useRegisterMutation } from "@/service/vaxsul";
-import { SerializedError } from "@reduxjs/toolkit";
-import { FetchBaseQueryError } from "@reduxjs/toolkit/query";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
+import Link from 'next/link';
 
 export default function Register() {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
+  const [fullName, setFullName] = useState('');
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [state, setState] = useState('');
+  const [city, setCity] = useState('');
+
   const [register, result] = useRegisterMutation();
   const router = useRouter();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (password !== confirmPassword) {
+      alert("As senhas não coincidem. Por favor, verifique.");
+      return;
+    }
+
+    register({
+      username: username,
+      password: await sha256(password),
+      fullName: fullName,
+      email: email,
+      state: state,
+      city: city,
+    })
+      .unwrap()
+      .then(() => router.push("/login"))
+      .catch((error) => {
+        console.error("Erro ao registrar:", error);
+      });
+
+    setFullName('');
+    setUsername('');
+    setEmail('');
+    setPassword('');
+    setConfirmPassword('');
+    setState('');
+    setCity('');
+  };
+
   return (
-    <form
-      className="max-w-sm mx-auto flex justify-center items-center h-screen"
-      onSubmit={async (e) => {
-        e.preventDefault();
-        register({
-          username: username,
-          password: await sha256(password),
-        })
-          .unwrap()
-          .catch()
-          .then(() => router.push("/login"));
-      }}
-    >
-      <div className="w-full">
-        {result.isError && (
-          <div
-            className="p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400"
-            role="alert"
+    <div className="min-h-screen flex flex-col items-center justify-center"
+      style={{ background: 'linear-gradient(to bottom, rgba(30, 66, 111, 0.9), rgba(30, 66, 111, 0.5))' }}>
+      <div className="absolute top-0 right-0 p-4 flex items-center">
+        <span className="text-white mr-2">Já tem uma conta?</span>
+        <Link href="/login">
+          <button
+            className="text-white bg-white bg-opacity-20 hover:bg-opacity-30 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 transition duration-150 ease-in-out"
           >
-            {getMessageForStatusCode(result.error)}
-          </div>
-        )}
-        <div className="mb-5">
-          <label
-            htmlFor="email"
-            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-          >
-            E-mail
-          </label>
-          <input
-            type="email"
-            id="email"
-            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            placeholder="example@email.com"
-            required
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-          />
-        </div>
-        <div className="mb-5">
-          <label
-            htmlFor="password"
-            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-          >
-            Senha
-          </label>
-          <input
-            type="password"
-            id="password"
-            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            required
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </div>
-        <div className="flex items-start mb-5">
-          <div className="flex items-center h-5">
+            Entrra
+          </button>
+        </Link>
+      </div>
+      <div className="w-4/5 max-w-lg bg-white bg-opacity-30 p-6 rounded-lg shadow-lg mt-10 mb-20">
+        <h2 className="text-2xl font-bold text-gray-200 mb-6 text-center">Cadastro</h2>
+        <form
+          onSubmit={handleSubmit}
+          className="space-y-4"
+        >
+          {result.isError && (
+            <div
+              className="p-4 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400"
+              role="alert"
+            >
+              {getMessageForStatusCode(result.error)}
+            </div>
+          )}
+          <div className="flex flex-col">
+            <label
+              htmlFor="fullName"
+              className="text-sm font-medium text-gray-200 mb-1"
+            >
+              Nome Completo
+            </label>
             <input
-              id="remember"
-              type="checkbox"
-              value=""
-              className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800"
+              type="text"
+              id="fullName"
+              className="bg-gray-50 bg-opacity-40 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-3 placeholder-gray-500"
+              placeholder="Nome completo"
+              required
+              value={fullName}
+              onChange={(e) => setFullName(e.target.value)}
             />
           </div>
-          <label
-            htmlFor="remember"
-            className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+          <div className="flex flex-col">
+            <label
+              htmlFor="email"
+              className="text-sm font-medium text-gray-200 mb-1"
+            >
+              E-mail
+            </label>
+            <input
+              type="email"
+              id="email"
+              className="bg-gray-50 bg-opacity-40 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-3 placeholder-gray-500"
+              placeholder="example@email.com"
+              required
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div className="flex flex-col">
+            <label
+              htmlFor="password"
+              className="text-sm font-medium text-gray-200 mb-1"
+            >
+              Senha
+            </label>
+            <input
+              type="password"
+              id="password"
+              className="bg-gray-50 bg-opacity-40 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-3 placeholder-gray-500"
+              required
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <div className="flex flex-col">
+            <label
+              htmlFor="confirmPassword"
+              className="text-sm font-medium text-gray-200 mb-1"
+            >
+              Confirme a Senha
+            </label>
+            <input
+              type="password"
+              id="confirmPassword"
+              className="bg-gray-50 bg-opacity-40 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-3 placeholder-gray-500"
+              required
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+            />
+          </div>
+          <div className="flex flex-col">
+            <label
+              htmlFor="state"
+              className="text-sm font-medium text-gray-200 mb-1"
+            >
+              Estado
+            </label>
+            <select
+              id="state"
+              className="bg-gray-50 bg-opacity-40 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-3 placeholder-gray-500"
+              value={state}
+              onChange={(e) => setState(e.target.value)}
+              required
+            >
+              <option value="">Selecione o Estado</option>
+              <option value="RS">Rio Grande do Sul</option>
+              <option value="SP">São Paulo</option>
+              <option value="MG">Minas Gerais</option>
+              <option value="RJ">Rio de Janeiro</option>
+            </select>
+          </div>
+          <div className="flex flex-col">
+            <label
+              htmlFor="city"
+              className="text-sm font-medium text-gray-200 mb-1"
+            >
+              Cidade
+            </label>
+            <select
+              id="city"
+              className="bg-gray-50 bg-opacity-40 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-3 placeholder-gray-500"
+              value={city}
+              onChange={(e) => setCity(e.target.value)}
+              required
+            >
+              <option value="">Selecione a Cidade</option>
+              <option value="Porto Alegre">Porto Alegre</option>
+              <option value="São Paulo">São Paulo</option>
+              <option value="Belo Horizonte">Belo Horizonte</option>
+              <option value="Rio de Janeiro">Rio de Janeiro</option>
+            </select>
+          </div>
+          <button
+            type="submit"
+            className="text-white bg-[#9dca46] bg-opacity-90 hover:bg-opacity-50 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full px-5 py-3 text-center transition duration-150 ease-in-out transform hover:scale-105"
           >
-            Remember me
-          </label>
-        </div>
-        <button
-          type="submit"
-          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-        >
-          Enviar
-        </button>
+            Cadastrar
+          </button>
+        </form>
       </div>
-    </form>
+    </div>
   );
 }
 
-function getMessageForStatusCode(error: FetchBaseQueryError | SerializedError) {
+function getMessageForStatusCode(error) {
   if (
     "status" in error &&
-    typeof (error as FetchBaseQueryError).status === "number" &&
-    ((error as FetchBaseQueryError).status as number) == 400
+    typeof error.status === "number" &&
+    error.status === 400
   ) {
     return "Usuário já existente. Por favor, utilize algo diferente.";
   }
